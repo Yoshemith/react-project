0040e5e60e643a681e32dcb37768de46e968c098
Yoshemith <cv3227@innovaccion.mx>
2021-12-07 22:30:31 -0600

    Fetching movie genres from local API

--- src/components/GenresInDb.js
+++ src/components/GenresInDb.js
@@ -1,71 +1,52 @@
-import React from "react";
+import React, {Component} from 'react';
+import Genre  from './Genre';
 
-function LastMovieInDb() {
-  return (
-    <div className="col-lg-6 mb-4">
-      <div className="card shadow mb-4">
-        <div className="card-header py-3">
-          <h5 className="m-0 font-weight-bold text-gray-800">
-            Genres in Data Base
-          </h5>
-        </div>
-        <div className="card-body">
-          <div className="row">
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Acción</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Animación</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Aventura</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Ciencia Ficción</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Comedia</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Documental</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Drama</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Fantasia</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Infantiles</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Musical</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+class GenresInDb extends Component{
+    constructor(){
+        super()
+        this.state ={
+            genresList : []
+        }
+    }
+
+    componentDidMount(){
+        fetch('/api/genres')
+        .then(respuesta =>{
+            return respuesta.json()
+        })
+        .then(genres =>{
+            //console.log(genres)
+            this.setState({genresList: genres.data})
+        })
+        .catch(error => console.log(error))
+    }
+
+    mouseSobre(){
+        let caja = document.querySelector('.fondoCaja');
+        caja.classList.toggle("bg-secondary"); 
+    }
+
+    render(){
+        return (
+            <React.Fragment>
+                    {/*<!-- Categories in DB -->*/}
+                    <div className="col-lg-6 mb-4">						
+                        <div className="card shadow mb-4">
+                            <div className="card-header py-3 fondoCaja">
+                                <h6 onMouseOver = { () => this.mouseSobre() } className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h6>
+                            </div>
+                            <div className="card-body">
+                                <div className="row">
+                                    {this.state.genresList.map((genre, index)=>{
+                                        return  <Genre  {...genre}  key={index} />
+                                    })}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            
+            </React.Fragment>
+        )
+    }
 }
-
-export default LastMovieInDb;
+export default GenresInDb;
